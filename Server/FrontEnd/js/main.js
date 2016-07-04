cadf64f4b0dfedfb13a0689f6eb0904c76f5a993
Satabdi Ganguly <satabdi.ganguly89@gmail.com>
2016-07-04 17:40:08 +0200

    Adding scribble Map API

--- Server/FrontEnd/js/main.js
+++ Server/FrontEnd/js/main.js
@@ -2,3 +2,7 @@
  * Created by tasu on 04.07.16.
  */
 var socket=io.connect();
+
+    window.onload = function() {
+        var sm = new scribblemaps.ScribbleMap(document.getElementById('ScribbleMap'));
+    }
