0334d9dac9b8d93cc2229c5e5a272b8ed5493d1b
smajida <s.majid.azimi@gmail.com>
2016-08-26 01:00:24 +0200

    resize image added

--- server.js
+++ server.js
@@ -497,26 +497,11 @@ app.post('/userdetailssave', function(req, res){
 });
 
 
+//////*******************************START COMPUTER VISION*********************************************//////
+//////*******************************START COMPUTER VISION*********************************************//////
+//////*******************************START COMPUTER VISION*********************************************//////
+//////*******************************START COMPUTER VISION*********************************************//////
 
-//******** Face Smile Detection *********
-//******** Face Smile Detection *********
-//******** Face Smile Detection *********
-
-const SmileFaceDetector = require('./computerVision/SmileFaceDetector');
-//const detector = new SmileFaceDetector({smileScale: 1.01, smileNeighbor: 10});
-
-
-const detector = new SmileFaceDetector({
-  // Parameter specifying how much the image size is reduced at each image scale on face detection default: 1.05 
-  faceScale: 1.01,
-  // Parameter specifying how many neighbors each candidate rectangle should have to retain it on face detection default: 8 
-  faceNeighbor: 2,
-  // Parameter specifying how much the image size is reduced at each image scale on smile detection default: 1.7 
-  smileScale: 1.01,
-  // Parameter specifying how many neighbors each candidate rectangle should have to retain it on smile detection default: 22 
-  smileNeighbor: 2,
-  //I will adapt the parameters as the dataset grows.
-});
 
 
 app.post('/facesmiledetection',function(req,res){
@@ -541,11 +526,31 @@ app.post('/facesmiledetection',function(req,res){
       var _imagename= message.body.filename;
       var _imagepath= message.body.pathid;
 
+//******** Detection *********
+
+const SmileFaceDetector = require('./computerVision/SmileFaceDetector');
+//const detector = new SmileFaceDetector({smileScale: 1.01, smileNeighbor: 10});
+
+
+const detector = new SmileFaceDetector({
+  // Parameter specifying how much the image size is reduced at each image scale on face detection default: 1.05 
+  faceScale: 1.01,
+  // Parameter specifying how many neighbors each candidate rectangle should have to retain it on face detection default: 8 
+  faceNeighbor: 2,
+  // Parameter specifying how much the image size is reduced at each image scale on smile detection default: 1.7 
+  smileScale: 1.01,
+  // Parameter specifying how many neighbors each candidate rectangle should have to retain it on smile detection default: 22 
+  smileNeighbor: 2,
+  //I will adapt the parameters as the dataset grows.
+});
+
+
 
 /// start detecting faces for each image.
 detector.on('error', (error) => {
   console.error(error);
 });
+
 detector.on('face', (faces, image) => {
   console.log(faces);
   faces.forEach((face) => {
@@ -561,13 +566,16 @@ detector.on('face', (faces, image) => {
     })
 
 });
+
 //start detecting smiles for each image.
+
 detector.on('smile', (smiles, face, image) => {
   console.log(smiles);
   smiles.forEach((smile) => {
     console.log("found smile!")
     smilevar=1;
   });
+
     connect.addface('mongodb://localhost:27017/testimages','storeimages',_mapdataversionid, _markerid,_imagename,_imagepath,_userid,_mapid,smilevar,function(message){
       console.log("Message"+message);
       if(message == "yes")
@@ -578,6 +586,7 @@ detector.on('smile', (smiles, face, image) => {
 
 
 });
+
 detector.load(path.join(_imagepath,_imagename)).then((image) => {
   detector.detect(image);
 }).catch((e) => {
@@ -597,6 +606,13 @@ detector.load(path.join(_imagepath,_imagename)).then((image) => {
 
     });
 });
+
+
+//////*******************************END COMPUTER VISION*********************************************//////
+//////*******************************END COMPUTER VISION*********************************************//////
+//////*******************************END COMPUTER VISION*********************************************//////
+//////*******************************END COMPUTER VISION*********************************************//////
+
 
 
 //******** Socket Function to receive face and smile data *********
