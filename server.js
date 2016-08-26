a4242811e1384222c25a9429bda602a7be984bdb
SatabdiG <satabdi.ganguly89@gmail.com>
2016-08-26 15:32:27 +0200

    Computer Vision  code runs

--- server.js
+++ server.js
@@ -648,94 +648,76 @@ app.post('/userdetailssave', function(req, res){
 
 app.post('/facesmiledetection',function(req,res){
     upload(req,res,function(err) {
-      var _userid = req.body.userid;
-      var _mapid= req.body.mapid;
-      var _mapdataversionid =  req.body.mapdataversionid;
-      var _markerid = req.body.markerid;
-      var _imagename= req.body.filename;
-      if(__userid == "guest")
+      var userid = req.body.userid;
+      var mapid= req.body.mapid;
+      var mapdataversionid =  req.body.mapdataversionid;
+      var markerid = req.body.markerid;
+      var imagename= req.body.filename;
+      if(userid == "guest")
       {
-        _imagename=__dirname+'/uploads/guest';
-        _mapid="guestmap";
-        _mapdataversionid="guestid";
+         imagename=__dirname+'/uploads/guest';
+         mapid="guestmap";
+         mapdataversionid="guestid";
       }
-      if(__userid != "guest") {
-        connect.retrievevalues('mongodb://localhost:27017/testimages', 'usercollection', _mapdataversionid, _markerid,_imagename, _imagepath,_userid,_mapid, function(message){
-      var _userid = message.body.userid;
-      var _mapid= message.body.mapid;
-      var _mapdataversionid =  message.body.mapdataversionid;
-      var _markerid = message.body.markerid;
-      var _imagename= message.body.filename;
-      var _imagepath= message.body.pathid;
-
-//******** Detection *********
-
-const SmileFaceDetector = require('./computerVision/SmileFaceDetector');
-//const detector = new SmileFaceDetector({smileScale: 1.01, smileNeighbor: 10});
+      if(userid != "guest") {
+      connect.getPictures('mongodb://localhost:27017/testimages',userid,mapid, function(imagename, imagepath,mapid){
+        if(imagename!= undefined || imagepath!= undefined) {
+          var imagename = imagename;
+          var imagepath = path.join(__dirname,imagepath);
+          console.log("Path is"+imagepath)
 
 
-const detector = new SmileFaceDetector({
-  // Parameter specifying how much the image size is reduced at each image scale on face detection default: 1.05 
-  faceScale: 1.01,
-  // Parameter specifying how many neighbors each candidate rectangle should have to retain it on face detection default: 8 
-  faceNeighbor: 2,
-  // Parameter specifying how much the image size is reduced at each image scale on smile detection default: 1.7 
-  smileScale: 1.01,
-  // Parameter specifying how many neighbors each candidate rectangle should have to retain it on smile detection default: 22 
-  smileNeighbor: 2,
-  //I will adapt the parameters as the dataset grows.
-});
+          //******** Detection *********
+          /// start detecting faces for each image.
+          detector.on('error', (error) => {
+            console.error(error);
+          });
 
+          detector.on('face', (faces, image) => {
+            console.log(faces);
+            faces.forEach((face) => {
+              console.log("found face!")
+              facevar = 1;
+            });
+            /*
+             connect.addface('mongodb://localhost:27017/testimages','storeimages',_mapdataversionid, _markerid,_imagename,_imagepath,_userid,_mapid,facevar,function(message){
+             console.log("Message"+message);
+             if(message == "yes")
+             return res.end("yes");
+             else
+             return res.end("no");
+             })
+             */
+          });
 
+          //start detecting smiles for each image.
 
-/// start detecting faces for each image.
-detector.on('error', (error) => {
-  console.error(error);
-});
+          detector.on('smile', (smiles, face, image) => {
+            console.log(smiles);
+            smiles.forEach((smile) => {
+              console.log("found smile!")
+              smilevar = 1;
+            });
 
-detector.on('face', (faces, image) => {
-  console.log(faces);
-  faces.forEach((face) => {
-    console.log("found face!")  
-    facevar=1;
-  });
-    connect.addface('mongodb://localhost:27017/testimages','storeimages',_mapdataversionid, _markerid,_imagename,_imagepath,_userid,_mapid,facevar,function(message){
-      console.log("Message"+message);
-      if(message == "yes")
-        return res.end("yes");
-      else
-        return res.end("no");
-    })
+            /*
+             connect.addface('mongodb://localhost:27017/testimages','storeimages',_mapdataversionid, _markerid,_imagename,_imagepath,_userid,_mapid,smilevar,function(message){
+             console.log("Message"+message);
+             if(message == "yes")
+             return res.end("yes");
+             else
+             return res.end("no");
+             })
+             */
 
-});
+          });
 
-//start detecting smiles for each image.
-
-detector.on('smile', (smiles, face, image) => {
-  console.log(smiles);
-  smiles.forEach((smile) => {
-    console.log("found smile!")
-    smilevar=1;
-  });
-
-    connect.addface('mongodb://localhost:27017/testimages','storeimages',_mapdataversionid, _markerid,_imagename,_imagepath,_userid,_mapid,smilevar,function(message){
-      console.log("Message"+message);
-      if(message == "yes")
-        return res.end("yes");
-      else
-        return res.end("no");
-    })
-
-
-});
-
-detector.load(path.join(_imagepath,_imagename)).then((image) => {
-  detector.detect(image);
-}).catch((e) => {
-  console.error(e);
-});
-
-})
+          detector.load(path.join(imagepath, imagename)).then((image) => {
+            detector.detect(image);
+          }).catch((e) => {
+            console.error(e);
+          });
+        }
+      })
       }
       else
       {
